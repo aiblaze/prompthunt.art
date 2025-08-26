714b9fdb17633ed24fbc2c6a6bbc24eb6461727f
Levin <mamboer@live.com>
2025-08-26 17:39:06 +0800

    feat: Update footer to reflect new organization name and description in localization files

--- app/components/App/Footer.vue
+++ app/components/App/Footer.vue
@@ -3,10 +3,10 @@
     class="w-full max-w-2xl mx-auto text-neutral-400 dark:text-neutral-600 text-sm text-center pb-8 pt-20"
   >
     <p class="mt-2">
-      &copy; 2025 - FOREVER {{ $t('author.name') }}. {{ $t('labels.allRightsReserved') }}.
+      &copy; 2025 - FOREVER {{ $t('org.name') }}. {{ $t('labels.allRightsReserved') }}.
     </p>
     <div class="mt-1 text-xs">
-      <a href="http://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">粤ICP备2021128240-2号</a>
+      <a href="http://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">粤ICP备2021128240号-2</a>
     </div>
   </footer>
 </template>
